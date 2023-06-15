4f0136cd0d14e54c84f2b879c1303e7d336f5953
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-06-15 11:44:14 +0900

    23.06.15 11:43 Product, RawMaterial, Requirements, Warehouse, Line, WorkOrder, Performance VO들,,lombok적용함

--- shoesMonster/src/main/java/com/sm/domain/ProductVO.java
+++
@@ -1,9 +0,0 @@
-package com.sm.domain;
-
-import lombok.Data;
-import lombok.Getter;
-
-@Getter
-public class ProductVO {
-	private String prod_code;
-}
---
+++ shoesMonster/src/main/java/com/sm/domain/a/ProductVO.java
@@ -0,0 +1,18 @@
+package com.sm.domain.a;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+	private String prod_code;
+	private String prod_name;
+	private String prod_category;
+	private String prod_unit;
+	private String prod_size;
+	private String prod_color;
+	private String client_code;
+	private float prod_price;
+	private String prod_note;
+	
+	
+} //productVO
