d099f6132d9e1a377a916f9cd172679f6c3ddecd
ckddnjs1653 <ckddnjs1653@naver.com>
2023-06-16 19:26:20 +0900

    06/16 19:26

--- shoesMonster/src/main/java/com/sm/controller/stockController.java
+++ shoesMonster/src/main/java/com/sm/controller/stockController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sm.domain.In_materialVO;
+import com.sm.domain.Raw_orderVO;
 import com.sm.service.In_materialService;
+import com.sm.service.Raw_orderService;
 
 @Controller
 @RequestMapping(value = "/stock/*")
@@ -23,23 +25,27 @@ public class stockController {
 	@Autowired
 	private In_materialService service;
 	
+	@Autowired
+	private Raw_orderService ro_service;
+	
 	private static final Logger logger = LoggerFactory.getLogger(stockController.class);
 	
-	// 발주 페이지로 이동 - /stock/raw_order
-	// http://localhost:8080/stock/raw_order
+	// 발주 목록 조회
+    // http://localhost:8080/stock/raw_order
 	// http://localhost:8088/stock/raw_order
-	@RequestMapping(value = "/raw_order", method = RequestMethod.GET)
-	public void raw_orderGET() throws Exception {
-		logger.debug("raw_orderGET() 호출");
-	}
-	// 발주 페이지로 이동 - /stock/raw_order
-	
-	// 발주 목록
-	public void raw_order_listGET() {
-		logger.debug("raw_order_listGET() 호출");
-		
-	}
-	// 발주 목록
+    @RequestMapping(value = "/raw_order", method = RequestMethod.GET)
+    public void ro_ListGET(Model model) throws Exception {
+        logger.debug("ro_ListGET() 호출");
+        
+        // Service - DB에 저장된 발주 목록 가져오기
+        List<Raw_orderVO> ro_List = ro_service.getRaw_Order_List();
+        logger.debug("ro_List : " + ro_List);
+        
+        model.addAttribute("ro_List", ro_List);
+        
+//        return "/stock/raw_order";
+    }
+    // 발주 목록 조회
 	
 	
 	
--- shoesMonster/src/main/java/com/sm/persistence/Raw_orderImpl.java
+++ shoesMonster/src/main/java/com/sm/persistence/Raw_orderImpl.java
@@ -2,16 +2,30 @@ package com.sm.persistence;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.sm.domain.Raw_orderVO;
 
+@Repository
 public class Raw_orderImpl implements Raw_orderDAO{
-
-	@Override
-	public List<Raw_orderVO> readRaw_Order() throws Exception {
-		
-		return null;
-	}
-	
-	
+    
+    @Autowired
+    private SqlSession sqlSession;
+    
+    private static final Logger logger = LoggerFactory.getLogger(Raw_orderImpl.class);
+    
+    private static final String NAMESPACE="com.sm.mapper.stockMapper";
+    
+    
+    @Override
+    public List<Raw_orderVO> readRaw_Order() throws Exception {
+        logger.debug("발주 목록 조회 mapper 호출");
+        
+        return sqlSession.selectList(NAMESPACE + ".getRaw_Order");
+    }
 
 }
