1ae00b511dcc98edb5af1ae666d234e50d0cfef6
LG <LG@dori>
2023-06-16 16:20:53 +0900

    23-06-16 16:20

--- shoesMonster/src/main/java/com/sm/domain/ClientsVO.java
+++ shoesMonster/src/main/java/com/sm/domain/ClientsVO.java
@@ -1,5 +1,24 @@
 package com.sm.domain;
 
+import lombok.Data;
+
+@Data
 public class ClientsVO {
 
-}
+	private String client_code;
+	private String client_actname;
+	private String client_type;
+	private String client_number;
+	private String client_sort;
+	private String client_ceo;
+	private String client_name;
+	private String client_addr;
+	private String client_addr2;
+	private String client_tel;
+	private String client_phone;
+	private String client_fax;
+	private String client_email;
+	private String client_note;
+	private String client_useyn;
+	
+} // ClientsVO
---
+++ shoesMonster/src/main/java/com/sm/persistence/ClientsDAOImpl.java
@@ -0,0 +1,5 @@
+package com.sm.persistence;
+
+public class ClientsDAOImpl implements ClientsDAO {
+	
+}
