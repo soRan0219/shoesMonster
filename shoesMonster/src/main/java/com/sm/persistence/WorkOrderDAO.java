dff4b9d93128f81367d10f4c9c62d10b186a00e8
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-06-18 23:20:55 +0900

    23.06.18 23:19 작업지시 목록 불러오기, 추가하기, 선택 삭제하기 (+ pom.xml에 gson 라이브러리 추가)

--- shoesMonster/src/main/java/com/sm/persistence/WorkOrderDAO.java
+++ shoesMonster/src/main/java/com/sm/persistence/WorkOrderDAO.java
@@ -9,4 +9,10 @@ public interface WorkOrderDAO {
 	//작업지시 목록
 	public List<WorkOrderVO> readAllWorkOrder() throws Exception;
 	
+	//작업지시 등록
+	public void insertWorkOrder(WorkOrderVO vo) throws Exception;
+	
+	//작업지시 삭제
+	public void deleteWorkOrder(List<String> checked) throws Exception;
+	
 } //WorkOrderDAO
