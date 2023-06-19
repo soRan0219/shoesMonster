43776cd2ab5c265288b6ee018e9d795be5923295
eon2258 <eon2258@naver.com>
2023-06-19 12:04:10 +0900

    23-06-19

--- shoesMonster/src/main/java/com/sm/service/PerformanceService.java
+++ shoesMonster/src/main/java/com/sm/service/PerformanceService.java
@@ -1,16 +1,19 @@
 package com.sm.service;
 
 import java.util.List;
-import com.sm.domain.ProductList;
-import com.sm.domain.ProductVO;
+
 import com.sm.domain.LineVO;
+import com.sm.domain.ProductVO;
 
 public interface PerformanceService {
 		
 	// 품목관리 리스트 불러오기
 	public List<ProductVO> getProdList() throws Exception; 
-  
-  // 품목관리 정보 다중 저장
+	
+	// 품목관리 검색리스트 불러오기
+	public List<ProductVO> getProdList(ProductVO vo) throws Exception;
+	
+	// 품목관리 정보 다중 저장
 	public void insertProd(List<ProductVO> products); 
 
 	// 라인 
