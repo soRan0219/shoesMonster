7483d3eee1ad52ffe0a7a19a4cc85de843746d93
cocochichi <psi8090@naver.com>
2023-06-21 14:41:44 +0900

    230621 14:41 창고 목록 조회 완전 성공! feat.창원

--- shoesMonster/src/main/java/com/sm/controller/PerfomanceController.java
+++ shoesMonster/src/main/java/com/sm/controller/PerfomanceController.java
@@ -109,22 +109,9 @@ public class PerfomanceController {
 	//======== 창고 - /warehouse ===========================
 	// http://localhost:8088/performance/warehouse
 	@RequestMapping(value = "/warehouse", method = RequestMethod.GET)
-	public void warehouseGET(Model model, 
-					@RequestParam(required = false) String prod_code,
-					@RequestParam(required = false) String raw_code
-					, Wh_prodVO wh_prod) throws Exception{
+	public void warehouseGET(Model model) throws Exception{
 		
 		logger.debug("warehouseGET() 호출");
-		
-//		String itemName = null;
-//		
-//		if(prod_code == null && raw_code != null) {
-//			// raw_code 처리
-//			
-//			
-//		}else if(raw_code == null && prod_code != null) {
-//			
-//		}
 		
 		List<WarehouseVO> whList = service.getWhList();
 		model.addAttribute("whList", whList);
