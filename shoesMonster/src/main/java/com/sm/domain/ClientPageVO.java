74d1cb5fb9b905a139b3c11ea4026c4b8c00b52e
LG <LG@dori>
2023-06-28 17:57:53 +0900

    23-06-28 17:57 거래처 페이징 + 검색 겨우 성공

---
+++ shoesMonster/src/main/java/com/sm/domain/ClientPageVO.java
@@ -0,0 +1,49 @@
+package com.sm.domain;
+
+public class ClientPageVO {
+	private int page;
+	private int pageSize;
+	
+	public ClientPageVO() {
+		this.page = 1;
+//		this.pageSize = 10; // 잠시 수정
+		this.pageSize = 1; // 잠시 수정
+	}
+
+	// 제어 설정
+	public void setPage(int page) {
+		if(page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
+	}
+	
+	public void setPageSize(int pageSize) {
+		if(pageSize <= 0 || pageSize > 100) {
+//			this.pageSize = 10; // 잠시 수정
+			this.pageSize = 1; // 잠시 수정
+			return;
+		}
+		this.pageSize = pageSize;
+	}
+	
+	public int getPage() {
+		return page;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public int getStartPage() {
+		return (this.page - 1) * this.pageSize;
+	}
+
+	@Override
+	public String toString() {
+		return "ClientPageVO [page=" + page + ", pageSize=" + pageSize + "]";
+	}
+	
+	
+}
--- shoesMonster/src/main/java/com/sm/service/ClientsServiceImpl.java
+++ shoesMonster/src/main/java/com/sm/service/ClientsServiceImpl.java
@@ -1,10 +1,12 @@
 package com.sm.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sm.domain.ClientPageVO;
 import com.sm.domain.ClientsVO;
 import com.sm.persistence.ClientsDAO;
 
@@ -16,17 +18,27 @@ public class ClientsServiceImpl implements ClientsService {
 	
 	// 거래처 목록 불러오기
 	@Override
-	public List<ClientsVO> getListAll() throws Exception {
-		return cldao.readClientsListAll();
+	public List<ClientsVO> getListAll(ClientPageVO cpvo) throws Exception {
+		return cldao.readClientsListAll(cpvo);
 	}
 
 	// 거래처 검색
 	@Override
-	public List<ClientsVO> getSearchClientsList(ClientsVO cvo) throws Exception {
-		return cldao.getSearchClientsList(cvo);
+	public List<ClientsVO> getSearchClientsList(HashMap<String, Object> search) throws Exception {
+		return cldao.getSearchClientsList(search);
 	}
 	
+	// 거래처 전체 개수
+	@Override
+	public int getTotalClient() throws Exception {
+		return cldao.getTotalClient();
+	}
 	
+	// 거래처 검색 개수 
+	@Override
+	public int getSearchClient(HashMap<String, Object> search) throws Exception {
+		return cldao.getSearchClient(search);
+	}
 	
 
 }
