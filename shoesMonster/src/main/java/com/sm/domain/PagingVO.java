aebd64f5e1bd1bae9982839d3f574749803fc4d2
EGParkk <cooldjsgy@naver.com>
2023-07-09 14:38:57 +0900

    23-07-09
    14:38

--- shoesMonster/src/main/java/com/sm/domain/PagingVO.java
+++ shoesMonster/src/main/java/com/sm/domain/PagingVO.java
@@ -7,7 +7,7 @@ public class PagingVO {
 		// 현재페이지, 시작페이지, 끝페이지, 게시글 총 갯수, 페이지당 글 갯수, 마지막페이지, SQL쿼리에 쓸 start, end
 		private int startPage, endPage, total,lastPage, start, end;
 		private int nowPage=1;
-		private int cntPerPage = 8;
+		private int cntPerPage = 3;
 		// 페이지 블럭
 		private int cntPage = 5;
 		
